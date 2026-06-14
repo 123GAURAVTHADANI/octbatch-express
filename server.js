ce45455e3e2cd3ae1794df7a1156c03bab95822e
123GAURAVTHADANI <garv.thadani@gmail.com>
2026-06-14 21:18:06 +0530

    Changes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var dotenv = require("dotenv");
 const { getUser, createUser } = require("./controllers/user.controller");
+const { userRouter } = require("./routers/user.router");
+const {
+  ageCheckMiddleware,
+  adhaarCardCheckMiddleware,
+} = require("./middlewares/user.middleware");
+const { logger } = require("./middlewares/logger.middleware");
 
 dotenv.config();
 
@@ -8,13 +14,38 @@ var app = express();
 
 let port = process.env.PORT;
 
-app.get("/getUser", getUser);
-app.post("/createUser", createUser);
+// application level middleware
+app.use(logger);
+
+// Router level middleware
+app.use(
+  "/api/v1/user",
+  ageCheckMiddleware,
+  adhaarCardCheckMiddleware,
+  userRouter,
+);
+
+// app.use("/api/v1/order", orderRouter);
 
 app.listen(port, () => {
   console.log(`Listening to the port`);
 });
 
 // http://localhost:5001/api/v1/users/getUsers
+// http://localhost:5001/api/v1/users/createUser
+// http://localhost:5001/api/v1/order/getOrders
 
-// dsa - updates !!! 
+// dsa - updates !!!
+
+// scratch ; express app , order , user , payment routes
+
+// swiggy (backend)
+// getOrders
+// login
+
+// endpoint, list down that !!! (5 mins)
+
+// fetch('https://fakestoreapi.com/products', {
+// method: "POST", body : payload})
+// // .then(res=>res.json())
+// // .then(json=>console.log(json))
